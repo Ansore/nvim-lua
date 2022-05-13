e8c3ee8babc909ca6750b2da94016bf184b93b10
Ansore <aansore@outlook.com>
2022-05-13 23:41:18 +0800

    update

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -3,8 +3,6 @@ if not status_ok then
 	return
 end
 
--- local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
 local lspconfig = require("lspconfig")
 
 local opts = {
