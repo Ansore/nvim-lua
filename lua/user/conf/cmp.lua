3b42fe32408378ae9f4e7c85e83778356ed39d3c
Ansore <aansore@outlook.com>
2022-08-20 00:50:06 +0800

    cmp modify shortcut key

--- lua/user/conf/cmp.lua
+++ lua/user/conf/cmp.lua
@@ -114,11 +114,11 @@ CmpConfig = {
 		end,
 	},
 	window = {
-    documentation = false,
-    completion = {
-      border = "rounded",
-      winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
-    },
+		documentation = false,
+		completion = {
+			border = "rounded",
+			winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
+		},
 		-- completion = cmp.config.window.bordered(),
 		-- documentation = cmp.config.window.bordered(),
 	},
@@ -137,9 +137,10 @@ CmpConfig = {
 	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-j>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
+			-- if cmp.visible() then
+			-- 	cmp.select_next_item()
+			-- else
+			if luasnip.expand_or_jumpable() then
 				luasnip.expand_or_jump()
 			elseif has_words_before() then
 				cmp.complete()
@@ -148,9 +149,10 @@ CmpConfig = {
 			end
 		end, { "i", "s" }),
 		["<C-k>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
+			-- if cmp.visible() then
+			-- 	cmp.select_prev_item()
+			-- else
+			if luasnip.jumpable(-1) then
 				luasnip.jump(-1)
 			else
 				fallback()
@@ -189,8 +191,8 @@ CmpConfig = {
 			"s",
 		}),
 
-    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
 		["<m-o>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
 		["<C-c>"] = cmp.mapping({
 			i = cmp.mapping.abort(),
